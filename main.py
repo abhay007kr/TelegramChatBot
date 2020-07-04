e270a938fb6565ef0efdce2295dcd75322a2a176
abhay007kr <aksabhay007@gmail.com>
2020-07-04 18:52:42 +0530

    added Function to get Chat ID

--- main.py
+++ main.py
@@ -2,3 +2,9 @@
 
 url="https://api.telegram.org/bot1098854739:AAHZNQBQGtdSpi5pm1ZmEFI9i2Eg47-fFUg"
 
+#Function to get Chat ID
+
+def get_chat_id(update):
+    chat_id = update["message"]["chat"]["id"]
+    return chat_id
+
