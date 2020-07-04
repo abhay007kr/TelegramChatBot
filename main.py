54c89f25ac485e74b499eea35d4174036f126297
abhay007kr <aksabhay007@gmail.com>
2020-07-05 03:55:31 +0530

    Final

--- main.py
+++ main.py
@@ -1,35 +1,49 @@
-import requests
+from random import random
 
+import requests as requests
 url = "https://api.telegram.org/bot1098854739:AAHZNQBQGtdSpi5pm1ZmEFI9i2Eg47-fFUg"
 
 
-# Function to get Chat ID
-
+# function that get chat id
 def get_chat_id(update):
-    chat_id = update["message"]["chat"]["id"]
+    chat_id = update['message']["chat"]["id"]
     return chat_id
 
 
-# function to get message text
-
+# function that get message text
 def get_message_text(update):
     message_text = update["message"]["text"]
     return message_text
 
 
-# function to get last update
-
+# function that get last_update
 def last_update(req):
     response = requests.get(req + "getUpdates")
     response = response.json()
     result = response["result"]
     total_updates = len(result) - 1
-    return result(total_updates)
+    return result[total_updates]  # get last record message update
 
 
-# function to let bot send message
-
+# create function that let bot send message to user
 def send_message(chat_id, message_text):
-    params = {"chat_id": chat_id, "text": "message_text"}
+    params = {"chat_id": chat_id, "text": message_text}
     response = requests.post(url + "sendMessage", data=params)
     return response
+
+
+# main function reply message back
+def main():
+    update_id = last_update(url)["update_id"]
+    while True:
+        update = last_update(url)
+        if update_id == update["update_id"]:
+            if get_message_text(update).lower() == "hi" or get_message_text(update).lower() == "hello":
+                send_message(get_chat_id(update), 'Hello Welcome to our bot. Type "Play" to roll the dice!')
+            else:
+                send_message(get_chat_id(update), "Sorry Not Understand what you inputted:( I love you")
+            update_id += 1
+
+
+# calling the function to make it reply
+main()
